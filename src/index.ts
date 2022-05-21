6578fb16c5b26b7f1a85b4466391632a7b55efe0
chanwit-y <chanwit.yimneam@hotmail.com>
2022-05-21 20:13:56 +0700

    add type

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,42 @@
-console.log("Hello world")
+enum FlowType {
+  Declare,
+  Assign,
+  If,
+  Loop,
+}
+
+enum DataType {
+  String,
+  Number,
+  Boolean,
+}
+
+type PrimitiveType = string | number | boolean;
+
+type Declare = {
+  name: string;
+  dataType: DataType;
+  defaultValue?: PrimitiveType;
+};
+
+type Flow = {
+  type: FlowType;
+  declare?: Declare;
+};
+
+const flows: Flow[] = [
+  {
+    type: FlowType.Declare,
+    declare: {
+      name: "foo",
+      dataType: DataType.String,
+      defaultValue: "test"
+    } 
+  },
+];
+
+function main() {
+  console.log("Hello world");
+}
+
+main();
